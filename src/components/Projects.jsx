8709e0ae702af3ae82cd173181340e8a2fc07cca
Nikhilkrishnapk <nikhilkrishna903@gmail.com>
2025-02-01 23:29:58 +0530

    added some features in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,8 +19,11 @@ const Projects = () => {
               <h2>Netflix Clone-UI</h2>
               <p>I created a Netflix clone UI using HTML and CSS. Through this project, I gained a strong understanding of CSS, focusing on layout design and styling.</p>
               <div className='project-card-btn'>
-                <button className='live-btn'>Live Now</button>
-                <button className='git-btn'>Github Link</button>
+
+
+
+                <a target="_blank" href="https://net-flix-clonev1.netlify.app/"> <button className='live-btn'> Live Now</button>  </a>
+                <a target="_blank" href="https://github.com/Nikhilkrishnapk/Netflix-clone.git" ><button className='git-btn'>Github Link </button> </a>
               </div>
             </div>
             <div className="services-format">
@@ -31,8 +34,10 @@ const Projects = () => {
               <h2>Library Management System</h2>
               <p>I developed a Library Management System with a responsive design. This project includes CRUD operations.</p>
               <div className='project-card-btn'>
-                <button className='live-btn'>Live Now</button>
-                <button className='git-btn'>Github Link</button>
+
+
+                <a target="_blank" href="https://e-bookify.netlify.app/"> <button className='live-btn'> Live Now</button>  </a>
+                <a target="_blank" href="https://github.com/Nikhilkrishnapk/eBookify-using-REACT.git" ><button className='git-btn'>Github Link </button> </a>
               </div>
             </div>
             <div className="services-format">
@@ -43,8 +48,8 @@ const Projects = () => {
               <h2>Weather App</h2>
               <p>I built a Weather App that fetches real-time data using an API and dynamically displays images based on the current weather conditions.</p>
               <div className='project-card-btn'>
-                <button className='live-btn'>Live Now</button>
-                <button className='git-btn'>Github Link</button>
+               <a target="_blank" href="https://leafy-blancmange-19b7bf.netlify.app/"> <button className='live-btn'> Live Now</button>  </a>
+                 <a target="_blank" href="https://github.com/Nikhilkrishnapk/Weather-app-using-REACT.git" ><button className='git-btn'>Github Link </button> </a>
               </div>
             </div>
           </div>
